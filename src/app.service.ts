20d35d86a3b67b54666c92e70bdc203313a8ec4e
Pedro Nieto <pedronieto.2005@gmail.com>
2023-11-23 21:46:13 -0300

    chore: removed prettier from app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression, Timeout } from '@nestjs/schedule';
 import { PrismaService } from './database/PrismaService';
