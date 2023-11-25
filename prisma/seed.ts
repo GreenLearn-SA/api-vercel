a0a1f4a30e498a566593ed99059c84c560540aa0
Pedro Nieto <pedronieto.2005@gmail.com>
2023-11-25 18:47:54 -0300

    fix: fixed prisma seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import {PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
@@ -7,58 +7,68 @@ const prisma = new PrismaClient();
 async function main() {
   const users = [
     {
-        id: "ec071bc5-d803-4c7c-9131-320c656a71c0",
-        firstName: "Pedro",
-        lastName: "Nieto",
-        username: "PedrooNietoo",
-        email: "pedronieto.2005@gmail.com",
-        password: "SenhaForte123!",
-        isManager: false
-      },
-      {
-        id: "ec071bc5-d803-4c7c-9131-320c656a71c1",
-        firstName: "Ashlyn",
-        lastName: "Iero",
-        username: "AshlynIero",
-        email: "ashlynierow@gmail.com",
-        password: "SenhaForte123!",
-        isManager: false
-      },
-      {
-        id: "ec071bc5-d803-4c7c-9131-320c656a71c2",
-        firstName: "Bernardo",
-        lastName: "Mattos",
-        username: "BernardoMattos",
-        email: "bernardomtt2@gmail.com",
-        password: "SenhaForte123!",
-        isManager: false
-      },
-      {
-        id: "ec071bc5-d803-4c7c-9131-320c656a71c3",
-        firstName: "Usuário",
-        lastName: "Administrador",
-        username: "admin",
-        email: "admin@greenlearn.com",
-        password: "Admin12!",
-        isManager: false
-      },
+      id: 'ec071bc5-d803-4c7c-9131-320c656a71c0',
+      firstName: 'Pedro',
+      lastName: 'Nieto',
+      username: 'PedrooNietoo',
+      email: 'pedronieto.2005@gmail.com',
+      password: 'SenhaForte123!',
+      isManager: false,
+    },
+    {
+      id: 'ec071bc5-d803-4c7c-9131-320c656a71c1',
+      firstName: 'Ashlyn',
+      lastName: 'Iero',
+      username: 'AshlynIero',
+      email: 'ashlynierow@gmail.com',
+      password: 'SenhaForte123!',
+      isManager: false,
+    },
+    {
+      id: 'ec071bc5-d803-4c7c-9131-320c656a71c2',
+      firstName: 'Bernardo',
+      lastName: 'Mattos',
+      username: 'BernardoMattos',
+      email: 'bernardomtt2@gmail.com',
+      password: 'SenhaForte123!',
+      isManager: false,
+    },
+    {
+      id: 'ec071bc5-d803-4c7c-9131-320c656a71c3',
+      firstName: 'Usuário',
+      lastName: 'Administrador',
+      username: 'admin',
+      email: 'admin@greenlearn.com',
+      password: 'Admin12!',
+      isManager: false,
+    },
   ];
 
   for (const user of users) {
     const salt = await bcrypt.genSalt();
     const hash: string = await bcrypt.hash(user.password, salt);
-    await prisma.user.upsert({
-      where: { email: user.email },
-      update: {},
-      create: {
-        id: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        username: user.username,
-        email: user.email,
-        password: hash,
-      },
-    });
+
+    try {
+      await prisma.user.upsert({
+        where: { email: user.email },
+        update: {},
+        create: {
+          id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          username: user.username,
+          email: user.email,
+          password: hash,
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2002' && error.meta?.target.includes('username')) {
+        console.error(`Username "${user.username}" already exists. Skipping.`);
+      } else {
+        console.error(error);
+        process.exit(1);
+      }
+    }
   }
 }
 
